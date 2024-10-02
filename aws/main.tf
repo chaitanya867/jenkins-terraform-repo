720e9a8c256e7d3622e41d2a8b17cd4c5184a468
Chaitanya Chougule <67632729+chaitanya867@users.noreply.github.com>
2024-10-02 16:52:14 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,17 +1,8 @@
-provider "aws" {
-  region = "us-west-2"  # Change to your desired region
-}
+resource "aws_instance" "my_vm" {
+ ami           = var.ami //Ubuntu AMI
+ instance_type = var.instance_type
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"  # Change to a globally unique name
-  acl    = "private"                       # Set the ACL (Access Control List)
-
-  tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
-  }
-}
-
-output "bucket_id" {
-  value = aws_s3_bucket.my_bucket.id
+ tags = {
+   Name = var.name_tag,
+ }
 }
