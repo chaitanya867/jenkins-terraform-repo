11324d18cfb3c368d981b7fbf61cfc009aaf90df
Chaitanya Chougule <67632729+chaitanya867@users.noreply.github.com>
2024-10-04 17:16:45 +0530

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,17 +1,17 @@
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-0ea3c35c5c3284d82"
+   #default     = "ami-0ea3c35c5c3284d82"
 }
 
 variable "instance_type" {
    type        = string
    description = "Instance type"
-   default     = "t2.micro"
+   #default     = "t2.micro"
 }
 
 variable "name_tag" {
    type        = string
    description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
+   #default     = "My EC2 Instance"
 }
